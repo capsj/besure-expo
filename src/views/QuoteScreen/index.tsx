26d3fb33884b76bd48e3cf7d6482006f3f4c8431
Jeronimo Carlos <jeronimo.carlos@ing.austral.edu.ar>
2022-08-07 14:30:52 +0100

    Initial version

---
+++ src/views/QuoteScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from "react-native";
+
+const QuoteScreen = () => {
+    return (
+       <SafeAreaView style={{ flex: 1}}>
+          <Text>hola</Text>
+       </SafeAreaView>
+    );
+};
+
+export default QuoteScreen;
